f063864b7f9d895d3bdbe8e1dec7f25d923d7c62
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2023-09-05 20:20:30 +0530

    Create Accessing the API.py

---
+++ Accessing the API.py
@@ -0,0 +1,10 @@
+import openai
+
+openai.api_key = "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"
+
+response = openai.Completion.create(
+    model = "text-davinci-003",
+    prompt= "What is Openai API"
+)
+
+print(response["choices"][0]["text"])          # The Output is in form of JSON File and it is nested in lists so to get only text from those list we print the only string which is necessary 
