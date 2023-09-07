ce7404a5a12136d0203f43fe307e49ce45c24a7a
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2023-09-07 22:17:48 +0530

    Update Accessing the API.py

--- Accessing the API.py
+++ Accessing the API.py
@@ -2,6 +2,9 @@ import openai
 
 openai.api_key = "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"
 
+# Use openai.ChatCompletion to use the Chat Model of openai 
+# Images are also there in repository for the working and usage of Models
+
 response = openai.Completion.create(
     model = "text-davinci-003",
     prompt= "What is Openai API"
